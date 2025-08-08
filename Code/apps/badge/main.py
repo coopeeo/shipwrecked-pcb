baa82922530ea7a491d0596d3db5e6c649ad06d8
Max Kendall <66796793+mpkendall@users.noreply.github.com>
2025-08-07 22:23:05 -0400

    fix wrapping on default badge text

--- Code/apps/badge/main.py
+++ Code/apps/badge/main.py
@@ -5,7 +5,7 @@ class App(badge.BaseApp):
         me = badge.contacts.my_contact()
         if not me:
             badge.display.fill(1)
-            badge.display.nice_text("No contact\ninfo. Please add\nyourself to\ncontacts.", 0, 0, font=32)
+            badge.display.nice_text("No contact\ninfo. Please\nadd yourself\nto contacts.", 0, 0, font=32)
             badge.display.show()
             return
         self.logger.info(f"Rendering contact info for {me}")
